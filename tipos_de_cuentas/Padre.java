d37433139a55ca32484d138897c6b2bcf04ef981
clipclasico <orozcosan1990@gmail.com>
2025-09-10 19:14:29 -0600

    Añadir constructor en Padre

--- tipos_de_cuentas/Padre.java
+++ tipos_de_cuentas/Padre.java
@@ -4,4 +4,11 @@ public class Padre extends Usuario
 {
     private String idEstudianteVinculado;
     private boolean recibirNotificacionesEmergencia;
+
+    public Padre(String id, String nombre, String correo, String contasena, String telefono, String idEstudianteVinculado)
+    {
+        super(id, nombre, correo, contasena, TipoCuenta.PADRE, telefono);
+        this.idEstudianteVinculado = idEstudianteVinculado;
+        this.recibirNotificacionesEmergencia = true;
+    }
 }
