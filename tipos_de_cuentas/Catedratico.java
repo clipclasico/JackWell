2a66d4da04c572aa14e984192b7c08553bfbbc4c
clipclasico <orozcosan1990@gmail.com>
2025-09-10 19:21:23 -0600

    Agregar clase Catedrático

---
+++ tipos_de_cuentas/Catedratico.java
@@ -0,0 +1,9 @@
+package tipos_de_cuentas;
+
+import java.util.ArrayList;
+
+public class Catedratico extends Usuario
+{
+    private ArrayList<String> materias;
+    private boolean puedeRecibirAlertas;
+}
