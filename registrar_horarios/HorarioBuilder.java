a1fb6242c3e4cb7d63f0809c16fb2393d223cd95
clipclasico <orozcosan1990@gmail.com>
2025-09-10 21:19:28 -0600

    Agregar constructor en HorarioBuilder

--- registrar_horarios/HorarioBuilder.java
+++ registrar_horarios/HorarioBuilder.java
@@ -1,5 +1,7 @@
 package registrar_horarios;
 
+import java.time.DayOfWeek;
+
 public class HorarioBuilder
 {
     private Horario horario;
@@ -8,4 +10,21 @@ public class HorarioBuilder
     {
         this.horario = new Horario();
     }    
+
+    public HorarioBuilder agregarClase(String materia, DayOfWeek dia, String horaInicio, String horaFin, String edificio, String salon, String catedratico)
+    {
+        try 
+        {
+            LocalTime inicio = LocalTime.parse(horaInicio);
+            LocalTime fin = LocalTime.parse(horaFin);
+
+            Clase clase = new Clase(materia, dia, inicio, fin, edificio, salon, catedratico);
+            horario.agregarClase(clase);
+        } catch (Exception e) 
+        {
+            System.out.println("Error al agregar clase: " + e.getMessage());
+        }
+
+        return this;
+    }
 }
