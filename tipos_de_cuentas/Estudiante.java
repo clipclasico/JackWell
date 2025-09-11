84ea6d3e7dcc5280cf82c24e7a0e17931e518999
clipclasico <orozcosan1990@gmail.com>
2025-09-10 19:03:57 -0600

    Añadir atributos de Estudiante

---
+++ tipos_de_cuentas/Estudiante.java
@@ -0,0 +1,14 @@
+package tipos_de_cuentas;
+
+import java.util.ArrayList;
+
+
+public class Estudiante extends Usuario 
+{
+    private ArrayList<EntradasDiario> entradasDiario;
+    private Configuracion configuraciones;
+    private Racha racha;
+    private MascotaVirtual mascota;
+    private Horario horario;
+    private ArrayList<ContactoEmergencia> contactosEmergencia;
+}
