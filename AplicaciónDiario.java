60880a9b32099cff50472eb1e705e799f2856682
Luis P López <lop24415@uvg.edu.gt>
2025-09-12 16:48:20 -0600

    Update AplicaciónDiario.java

--- AplicaciónDiario.java
+++ AplicaciónDiario.java
@@ -39,6 +39,8 @@ public class AplicacionDiario {
                 }
                 case 4 -> {
                     System.out.println("Gracias por usar tu diario, no platicamos luego");
+                    salir = true; 
+
 
 
 
