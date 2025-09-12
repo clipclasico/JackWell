1a9751e748b66802e7a311defbee3ed8a19f56ba
Luis P López <lop24415@uvg.edu.gt>
2025-09-12 16:39:13 -0600

    Update AplicaciónDiario.java

--- AplicaciónDiario.java
+++ AplicaciónDiario.java
@@ -23,6 +23,9 @@ public class AplicacionDiario {
                     System.out.print("¿Cómo te sientes hoy? (Ej: Feliz, Triste, Ansioso): ");
                     String animo = scanner.nextLine();
                     System.out.println("Escribe tus pensamientos o lo que te pasó hoy:");
+                    String pensamientos = scanner.nextLine();
+
+
 
 
 
