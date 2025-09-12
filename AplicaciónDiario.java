d2ed899be361bf52473208ec527e4495b31e9e40
Luis P López <lop24415@uvg.edu.gt>
2025-09-12 16:33:31 -0600

    Update AplicaciónDiario.java

--- AplicaciónDiario.java
+++ AplicaciónDiario.java
@@ -2,3 +2,9 @@ import java.util.Scanner;
 
 public class AplicacionDiario {
     public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+        DiarioEmocional miDiario = new DiarioEmocional();
+        boolean salir = false;
+
+
+
