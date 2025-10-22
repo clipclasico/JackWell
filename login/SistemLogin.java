fb19346bc31ff8145454e5a21963c4d2652aedf2
clipclasico <orozcosan1990@gmail.com>
2025-10-22 11:29:19 -0600

    Crear clase SistemaLogin

---
+++ login/SistemLogin.java
@@ -0,0 +1,9 @@
+import tipos_de_cuentas.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SistemaLogin
+{
+    private List<Usuario> usuarios;
+    private Usuario usuarioActual;
+}
