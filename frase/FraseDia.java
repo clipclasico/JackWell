8e3f4840ab9934a8d216c855f9967d9d7610a406
clipclasico <orozcosan1990@gmail.com>
2025-10-22 10:45:07 -0600

    Crear clase FraseDia

---
+++ frase/FraseDia.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class FraseDia
+{
+    private List<String> frases;
+    private LocalDate fechaActual;
+    private Random random;
+}
