a1cdd5c1e05b04f4ac89c4c6b0168d44265a81b6
clipclasico <orozcosan1990@gmail.com>
2025-10-22 10:34:01 -0600

    Añadir constructor de la clase ContactoEmergencia

--- contactoEmergencia/ContactoEmergencia.java
+++ contactoEmergencia/ContactoEmergencia.java
@@ -5,4 +5,13 @@ public class ContactoEmergencia
     private String relacion;
     private String email;
     private boolean activo;
+
+    public ContactoEmergencia(String nombre, String telefono, String relacion, String email)
+    {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.relacion = relacion;
+        this.email = email;
+        this.activo = true;
+    }
 }
