895a7c6a20de6847ebcd70109cc7f6593b6fc451
clipclasico <orozcosan1990@gmail.com>
2025-10-22 19:47:05 -0600

    agregar clases extras

--- extras/Racha.java
+++ extras/Racha.java
@@ -1,5 +1,27 @@
 package extras;
 
-public class Racha {
+public class Racha
+{
+    private int diasConsecutivos;
+
+    public Racha()
+    {
+        this.diasConsecutivos = 0;
+    }
+
+    public int getDiasConsecutivos()
+    {
+        return diasConsecutivos;
+    }
+
+    public void incrementarRacha()
+    {
+        diasConsecutivos++;
+    }
+
+    public void reiniciarRacha()
+    {
+        diasConsecutivos = 0;
+    }
     
 }
