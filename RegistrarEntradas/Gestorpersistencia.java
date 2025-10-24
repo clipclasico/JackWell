9ab531cb8fcf0f1f96f63d576b00f772de0d7cc2
Luis P López <lop24415@uvg.edu.gt>
2025-10-23 21:57:29 -0600

    Update Gestorpersistencia.java

--- RegistrarEntradas/Gestorpersistencia.java
+++ RegistrarEntradas/Gestorpersistencia.java
@@ -3,3 +3,13 @@ package RegistrarEntradas;
 import java.io.*;
 
 public class GestorPersistencia {
+
+public static final String RUTA_ARCHIVO = "diario.dat";
+
+    public static void guardarDatos(Object datos) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(RUTA_ARCHIVO);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+          
+          oos.writeObject(datos);
+        }
+    }
