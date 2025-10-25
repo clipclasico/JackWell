04a88707b27047da248d07e5ce9136579e90b1b8
Luis P López <lop24415@uvg.edu.gt>
2025-10-25 16:42:06 -0600

    Update GestorContactos.java

--- contactoEmergencia/GestorContactos.java
+++ contactoEmergencia/GestorContactos.java
@@ -23,3 +23,31 @@ public GestorContactos() {
             System.out.println("No hay contactos de emergencia registrados.");
             return;
         }
+        System.out.println("\n--- LISTA DE CONTACTOS ---");
+        for (ContactoEmergencia c : contactos) {
+            System.out.println(c.obtenerInformacion());
+            System.out.println("--------------------------");
+        }
+    }
+
+    public ContactoEmergencia buscarContacto(String nombre) {
+        for (ContactoEmergencia c : contactos) {
+            if (c.getNombre().equalsIgnoreCase(nombre)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public boolean eliminarContacto(String nombre) {
+        ContactoEmergencia c = buscarContacto(nombre);
+        if (c != null) {
+            contactos.remove(c);
+            System.out.println("Contacto eliminado: " + nombre);
+            return true;
+        } else {
+            System.out.println("No se encontró el contacto: " + nombre);
+            return false;
+        }
+    }
+}
