a06a35565e2ecd3df3625223cc81389b58df5bd1
Luis P López <lop24415@uvg.edu.gt>
2025-10-25 16:40:08 -0600

    Update GestorContactos.java

--- contactoEmergencia/GestorContactos.java
+++ contactoEmergencia/GestorContactos.java
@@ -3,3 +3,8 @@ package contactoEmergencia;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
+public class GestorContactos implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    private List<ContactoEmergencia> contactos;
