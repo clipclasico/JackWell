e41ac43f58171144de5d0672fcc349dd5a9470d2
Luis P López <lop24415@uvg.edu.gt>
2025-10-25 17:03:28 -0600

    Update GestorPersistencia.java

--- login/GestorPersistencia.java
+++ login/GestorPersistencia.java
@@ -3,3 +3,27 @@ package login;
 import java.io.*;
 
 public class GestorPersistencia {
+  public static final String RUTA_ARCHIVO = "login.dat";
+
+    public static void guardarDatos(Object datos) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(RUTA_ARCHIVO);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            
+            oos.writeObject(datos);
+        }
+    }
+
+    public static Object cargarDatos() throws IOException, ClassNotFoundException {
+        File archivo = new File(RUTA_ARCHIVO);
+        if (!archivo.exists()) {
+            return null;
+        }
+
+        try (FileInputStream fis = new FileInputStream(RUTA_ARCHIVO);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            
+            Object datos = ois.readObject();
+            return datos;
+        }
+    }
+}
